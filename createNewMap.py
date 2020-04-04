c9bba279fb4330bf0fbbd02a5e90b318e4693e20
lpshkn <kofffsh@gmail.com>
2020-04-04 20:56:06 +0300

    Add the checking for incorrect input in edit lines
    For the working with alignment must use to VLayout which was defined in ui file. It contains fields like labels, edit lines and buttons.
    If paths are incorrect, then write an error message about it. In order to do it have to insert the error message after the corresponding edit line. So, it's necessary to know indices of edit lines. After the writing message, increment the index of second edit lines, because it shifts due to was inserted the error message. Also, the accept button must be disabled if at least one of the directories is incorrect.
    Incorrect_maps(troops)_directory is necessary flag in order to write the error message after the line of edit if it's incorrect at the moment.

--- createNewMap.py
+++ createNewMap.py
@@ -16,10 +16,18 @@ class CreateNewMap(QDialog, FORM_CLASS):
         self.setupUi(self)
         self.configure_ui()
 
+        # These values specify that paths were written in the edit line by user were incorrect.
+        self.incorrect_maps_directory = False
+        self.incorrect_troops_directory = False
+
+        # These values are necessary to find out indices of lines edit
+        # Initially they are known from the ui file
+        self.index_maps_line = 1
+        self.index_troops_line = 3
+
     def configure_ui(self):
         self.setWindowIcon(QIcon(os.path.join(ICONS_IMAGES_DIR, 'mainIcon.png')))
         self.setWindowModality(Qt.ApplicationModal)
-        self.setFixedSize(self.size())
 
     def enable_accept_button(self):
         if self.mapsDirectoryLine.text() and self.troopsDirectoryLine.text():
@@ -27,7 +35,7 @@ class CreateNewMap(QDialog, FORM_CLASS):
         else:
             self.acceptButton.setEnabled(False)
 
-    def maps_manage(self):
+    def files_manage(self):
         sender = self.sender()
 
         directory = QFileDialog.getExistingDirectory(self, caption="Открыть", directory="")
@@ -39,4 +47,20 @@ class CreateNewMap(QDialog, FORM_CLASS):
                 self.troopsDirectoryLine.setText(directory)
 
     def accept(self):
-        pass
+        mapsDirectory = self.mapsDirectoryLine.text()
+        troopsDirectory = self.troopsDirectoryLine.text()
+
+        if not os.path.isdir(mapsDirectory) and not self.incorrect_maps_directory:
+            self.error_maps_lbl = QLabel("Ошибка: данной директории не существует", self)
+            self.error_maps_lbl.setStyleSheet('QLabel { color : red; }')
+            self.VLayout.insertWidget(self.index_maps_line + 1, self.error_maps_lbl)
+            self.index_troops_line += 1
+            self.incorrect_maps_directory = True
+            self.acceptButton.setEnabled(False)
+
+        if not os.path.isdir(troopsDirectory) and not self.incorrect_troops_directory:
+            self.error_troops_lbl = QLabel("Ошибка: данной директории не существует", self)
+            self.error_troops_lbl.setStyleSheet('QLabel { color : red; }')
+            self.VLayout.insertWidget(self.index_troops_line + 1, self.error_troops_lbl)
+            self.incorrect_troops_directory = True
+            self.acceptButton.setEnabled(False)
