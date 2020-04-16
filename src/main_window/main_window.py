9f6403a3fd66b3f7f87a35f496c12fbad6bb3f65
lpshkn <kofffsh@gmail.com>
2020-04-16 20:38:07 +0300

    Add comments and description to the method of setting window's geometry
    Some methods were be renamed, because it was decided to separate functional to several methods.

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -1,3 +1,7 @@
+"""
+This module represents the class to work with the main window. There are methods to setup geometry, setup
+menu bar, widgets e.t.c.
+"""
 from os.path import dirname, join
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QMainWindow, QDesktopWidget, QAction, QMessageBox)
@@ -10,17 +14,25 @@ class MainWindow(QMainWindow):
         super().__init__()
         # Define the directory of ui's files
         self.ICONS_IMAGES_DIR = join(dirname(dirname(dirname(__file__))), 'resources', 'images', 'icons')
-        self.setupUi()
+        self.setup_ui()
 
-    def setup_screen_size(self):
+    def setup_geometry(self):
+        """
+        Setup geometry of the main window. It determines screen dimensions and a central point, then uses it to
+        setup particular geometry.
+        """
+
+        # Determine screen dimensions and central point
         desktop = QDesktopWidget()
         screen_size = desktop.availableGeometry()
         center_point = screen_size.center()
 
+        # Determine dimensions and subtract some pixels in order to the window will not out of bounds
         width = screen_size.width() - 100
         height = screen_size.height() - 100
         self.setGeometry(center_point.x() - width / 2, center_point.y() - height / 2, width, height)
 
+        # Setup minimal dimensions
         self.setMinimumHeight(600)
         self.setMinimumWidth(800)
 
@@ -65,11 +77,14 @@ class MainWindow(QMainWindow):
         info_message.frameGeometry().moveCenter(info_message.frameGeometry().center())
         info_message.show()
 
-    def setupUi(self):
-        self.setWindowTitle('Troops Review')
+    def setup_title_settings(self):
+        self.setWindowTitle('Troops review')
         self.setWindowIcon(QIcon(join(self.ICONS_IMAGES_DIR, 'mainIcon.png')))
         self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinMaxButtonsHint)
-        self.setup_screen_size()
 
+    def setup_ui(self):
+        self.setup_geometry()
+        self.setup_title_settings()
         self.setup_menu()
+
         self.showMaximized()
