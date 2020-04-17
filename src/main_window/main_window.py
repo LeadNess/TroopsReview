514ff5af4713f7897a32485acbbd4c96000ccb24
lpshkn <kofffsh@gmail.com>
2020-04-17 15:51:42 +0300

    Add descriptions to methods

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -4,7 +4,7 @@ menu bar, widgets e.t.c.
 """
 from os.path import dirname, join
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QMainWindow, QDesktopWidget, QAction, QMessageBox)
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget
 from PyQt5.QtGui import QIcon
 from menu import menu_bar
 from widgets.map_widget import CentralWidget
@@ -38,16 +38,25 @@ class MainWindow(QMainWindow):
         self.setMinimumWidth(800)
 
     def setup_menu(self):
+        """
+        Setup a menu bar using the class-wrapper
+        """
         menubar = menu_bar.MenuBar(self)
         self.setMenuBar(menubar)
         self.statusBar()
 
     def setup_title_settings(self):
+        """
+        Setup settings of the window's title as like the title, icon or any buttons
+        """
         self.setWindowTitle('Troops review')
         self.setWindowIcon(QIcon(join(self.ICONS_IMAGES_DIR, 'mainIcon.png')))
         self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinMaxButtonsHint)
 
     def setup_ui(self):
+        """
+        The main method which sets all settings and runs main process
+        """
         self.setup_geometry()
         self.setup_title_settings()
         self.setup_menu()
@@ -55,6 +64,13 @@ class MainWindow(QMainWindow):
         self.showMaximized()
 
     def setup_central_widget(self, map_filename: str, troops_directory: str):
+        """
+        It's a slot, that is called when all settings are correct and the accept button is pressed in the dialog
+        window of creating new map.
+
+        :param map_filename: filename of the map
+        :param troops_directory: path of the directory containing elements
+        """
         central_widget = CentralWidget(self, map_filename, troops_directory)
         self.setCentralWidget(central_widget)
         central_widget.show()
