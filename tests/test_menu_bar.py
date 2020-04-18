1685937e6122ef60f531c3613adada62c37539d7
lpshkn <kofffsh@gmail.com>
2020-04-18 18:14:51 +0300

    Add fixes to previous commit, add test to check the creating new file
    When the button of creating new file is pressed, the dialog window of creating that file must be appeared.

--- tests/test_menu_bar.py
+++ tests/test_menu_bar.py
@@ -28,14 +28,19 @@ class MenuBarTest(unittest.TestCase):
     @unittest.mock.patch('src.menu.menu_bar.MenuBar.CREATE_ICON_PATH', 'wrong_create')
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_incorrect_default_icons_filenames(self, mock_stdout):
-        # These paths aren't exist
-        MenuBar.CREATE_ICON_PATH = 'wrong_create'
-        MenuBar.OPEN_ICON_PATH = 'wrong_open'
-        MenuBar.ABOUT_ICON_PATH = 'wrong_about'
-
         # The constructor will check paths and will print errors to the console. Important to compare this
         # text in the console and that error message
-        self.menubar = MenuBar(self.main)
+        menubar = MenuBar(self.main)
         self.assertEqual(mock_stdout.getvalue(), f"Error: No such file: {MenuBar.CREATE_ICON_PATH}\n"
                                                  f"Error: No such file: {MenuBar.OPEN_ICON_PATH}\n"
                                                  f"Error: No such file: {MenuBar.ABOUT_ICON_PATH}\n")
+
+    def test_pressing_button_create_file(self):
+        # The dialog of creating new file doesn't exist at the moment
+        with self.assertRaises(AttributeError):
+            var = self.main.new_file_dialog
+
+        # Click the button of creating new file
+        self.new_file_button.trigger()
+        # And now that dialog exists
+        self.assertIsNotNone(self.main.new_file_dialog)
