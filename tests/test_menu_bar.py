c312fd266863ec952fdf3e8720852776dde2a08a
lpshkn <kofffsh@gmail.com>
2020-04-18 18:12:21 +0300

    Add menu buttons to the setUp method, transfer changing paths in the class to the mock patch
    Default paths is necessary to be in the mocks, because they'll change and it's important to recover them to default values.
    
    Define menu buttons. That fact eases to access for events of menu's buttons.

--- tests/test_menu_bar.py
+++ tests/test_menu_bar.py
@@ -9,7 +9,23 @@ class MenuBarTest(unittest.TestCase):
     def setUp(self) -> None:
         self.app = QApplication([])
         self.main = QMainWindow()
+        self.menubar = MenuBar(self.main)
 
+        # Define menu buttons (they are on top ot the menu bar)
+        self.file_menu = self.menubar.actions()[0].menu()
+        self.help_menu = self.menubar.actions()[1].menu()
+
+        # Define actions for the "File" menu (these actions appear then is putted the cursor in the "File" menu)
+        self.new_file_button = self.file_menu.actions()[0]
+        self.open_file_button = self.file_menu.actions()[1]
+
+        # Define actions for the "Help" menu (these actions appear then is putted the cursor in the "Help" menu)
+        self.about_button = self.help_menu.actions()[0]
+
+    # These paths aren't exist
+    @unittest.mock.patch('src.menu.menu_bar.MenuBar.ABOUT_ICON_PATH', 'wrong_about')
+    @unittest.mock.patch('src.menu.menu_bar.MenuBar.OPEN_ICON_PATH', 'wrong_open')
+    @unittest.mock.patch('src.menu.menu_bar.MenuBar.CREATE_ICON_PATH', 'wrong_create')
     @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
     def test_incorrect_default_icons_filenames(self, mock_stdout):
         # These paths aren't exist
