b537809aa750f48e8c7815bd0317733f14f28456
lpshkn <kofffsh@gmail.com>
2020-04-19 19:45:14 +0300

    Add tests to check for invalid filename, default flags and setting of window's modality

--- tests/test_new_file.py
+++ tests/test_new_file.py
@@ -19,5 +19,17 @@ class MyTestCase(unittest.TestCase):
         NewFileDialog(QMainWindow())
         self.assertEqual(io_string.getvalue(), 'Error: No such file: incorrect\\mainIcon.png\n')
 
+    @unittest.mock.patch("src.menu.new_file.NewFileDialog.FILENAME_UI", 'incorrect')
+    @unittest.mock.patch("sys.stdout", new_callable=io.StringIO)
+    def test_incorrect_io_file(self, io_string):
+        # When you create an instance, FileNotFoundError will be raised and sys.exit will be called
+        with self.assertRaises(SystemExit):
+            NewFileDialog(QMainWindow())
 
+    def test_default_flags(self):
+        dialog = NewFileDialog(QMainWindow())
+        self.assertFalse(dialog.incorrect_map_filename)
+        self.assertFalse(dialog.incorrect_troops_directory)
 
+    def test_setting_modality(self):
+        self.assertTrue(self.new_map.isModal())
